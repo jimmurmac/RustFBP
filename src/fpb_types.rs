9660a45a046d20ab30f3151ebff4c34d972ba201
Jim Murphy <jimmur@mac.om>
2020-07-17 09:55:19 -0600

    Checkp[oint

--- src/fpb_types.rs
+++ src/fpb_types.rs
@@ -2,42 +2,33 @@ use std::sync::mpsc::channel;
 use uuid::Uuid;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
+use std::sync::Arc;
+use std::borrow::Borrow;
+use std::sync::Mutex;
 
 // ----------------------------------------------------------------------------
 // enum MessageType
-// 
+//
 // Provide a way to differentiate different messages sent for processing
 // ----------------------------------------------------------------------------
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum MessageType {
     Data,       // This is a 'normal' data packet to be processed by a node
-    Config,     // This is a configuration packet used to configure a node     
-}
-
-// ----------------------------------------------------------------------------
-// enum NodeState
-// 
-// Enumerate the current state of a Node
-// ----------------------------------------------------------------------------
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum NodeState {
-    Quiescent,
-    Running,
+    Config,     // This is a configuration packet used to configure a node
 }
 
 // ----------------------------------------------------------------------------
 // struct IIDMessage<T>
-// 
+//
 // Define the standard data packet that will be sent to a node.  Curently it is
 // defined as a generic.  This would allow for different payload types.
 // ----------------------------------------------------------------------------
 
-//#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct IIDMessage {
     msg_type: MessageType,
-    payload: Option<String>,
+    payload: Option<&'static str>,
 }
 
 impl IIDMessage {
@@ -45,7 +36,7 @@ impl IIDMessage {
     pub fn new(msg_type: MessageType, payload: Option<String>) -> Self {
         IIDMessage {
             msg_type: msg_type,
-            payload: payload,
+            payload: Box::new(payload),
         }
     }
 
@@ -55,71 +46,66 @@ impl IIDMessage {
     }
 
     // Return a reference to the payload of a IIDMessage
-    pub fn payload(&self) -> Option<String> {
+    pub fn payload(&self) -> &Option<String> {
         if self.payload.is_none() {
-            return None
+            return &None;
         }
         else
         {
-           Some(self.payload.as_ref().unwrap().clone())
+            self.payload.as_ref()
+        }
+    }
+}
+
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+struct FPBNodeStruct<'a, 'b> {
+    name: Option<&'static str>,
+    uuid: Uuid,
+    tx: &Box<std::sync::mpsc::Sender<IIDMessage>>,
+    rx: &Box<std::sync::mpsc::Receiver<IIDMessage>>,
+    state: AtomicBool,
+    node_thread: Option<std::thread::JoinHandle<()>>,
+    output_vec: std::vec::Vec<std::sync::mpsc::Sender<Box<IIDMessage>>>
+}
+
+impl FPBNodeStruct {
+    //Constructor
+    pub fn new(name: Option<&'static str>) -> Self {
+        let (tx, rx) = channel::<Box<IIDMessage>>();
+        FPBNodeStruct {
+            name: name,
+            uuid: Uuid::new_v4(),
+            tx: Box::new(tx),
+            rx: Box::new(rx),
+            state: AtomicBool::new(false),
+            node_thread: None,
+            output_vec: Vec::<std::sync::mpsc::Sender<Box<IIDMessage>>>::new()
         }
     }
 }
 
 // ----------------------------------------------------------------------------
-// struct FPBNodeStruct
-// 
-// Define the basic structure of a FPBNode. Every Node should have one of 
-// these as it's data members
-// ----------------------------------------------------------------------------
-
-struct FPBNodeStruct {
-    name: Option<&'static str>,
-    uuid: Uuid,
-    sender: std::sync::mpsc::Sender<IIDMessage>,
-    receiver: std::sync::mpsc::Receiver<IIDMessage>,
-    state: AtomicBool,
-    node_thread: Option<std::thread::JoinHandle<()>>,
-    output_vec: std::vec::Vec<std::sync::mpsc::Sender<IIDMessage>>
-}
-
- impl FPBNodeStruct {
-     //Constructor
-     pub fn new(name: Option<&'static str>) -> Self {
-         let (s, r) = channel::<IIDMessage>();
-         FPBNodeStruct {
-             name: name,
-             uuid: Uuid::new_v4(),
-             sender: s,
-             receiver: r,
-             state: AtomicBool::new(false),
-             node_thread: None,
-             output_vec: Vec::<std::sync::mpsc::Sender<IIDMessage>>::new()
-         }
-     }
- }
-
-// ----------------------------------------------------------------------------
 // trait FPBNodeTrait
-// 
+//
 // Define a trait (interface) for a FPBNode.  These methods provide the means
 // for interacting with a node
 //
 //  members:
 //          To facilitate sharing in a composition system like Rust, a specific
-//          implementation of a FPBNode will 'have a' boxed (heap based) 
+//          implementation of a FPBNode will 'have a' boxed (heap based)
 //          FPBNodeStruct member.  This allows for sharing the basic member
 //          variable structure that is necessary for every Node.  All of the
 //          other member accessors are based upon getting this FPBNodeStruct
 //          and using that to obtain the specific variable
 //
-//  name:   Each node should have a name.  The field is an Option so that it 
+//  name:   Each node should have a name.  The field is an Option so that it
 //          may be None
 //
-//  uuid:   Each node will have a unique ID (uuid).  This can be used to 
+//  uuid:   Each node will have a unique ID (uuid).  This can be used to
 //          configure a network of nodes
 //
-//  node_state: 
+//  node_state:
 //          Returns the current state of the node.
 //
 //  input_channel:
@@ -135,26 +121,25 @@ struct FPBNodeStruct {
 //          processed.
 //
 //  add_receiver:
-//          A Node will output it's data to a vector of other nodes.  NOTE: 
+//          A Node will output it's data to a vector of other nodes.  NOTE:
 //          the default for a new node will be to have it's vector of receivers
 //          be empty which is the same as sending to /dev/null
 //
-//  start:  Each Node is a aynchronous processing node.  It runs on a thread. 
-//          if the node is quiescent, then a thread will be created and that 
+//  start:  Each Node is a aynchronous processing node.  It runs on a thread.
+//          if the node is quiescent, then a thread will be created and that
 //          thread will call the process_data method.  This will continue until
 //          the node is stopped.
 //
 //  stop:   Once a Node has started, this method will stop the processing thread
 //          which will stop all processing for this Node.
 //
-//  process_data: 
-//          This is the 'heart and soul' of a Node.  When start is called upon 
+//  process_data:
+//          This is the 'heart and soul' of a Node.  When start is called upon
 //          a Node, the process_data method will pull messages and process them
 //          as needed to fullfil the role of this node.
-//          
+//
 // ----------------------------------------------------------------------------
-#[derive(Send)]
-trait FPBNodeTrait {
+trait FPBNodeTrait<'a> {
 
     // Constructor
     //fn new(name: Option<&'static str>) -> Self;
@@ -164,7 +149,7 @@ trait FPBNodeTrait {
 
     // Accessor to get the name of a Node
     fn name(&mut self) -> Option<&'static str> {
-      self.members().name
+        self.members().name
     }
 
     // Accessor to get the uuid of a Node
@@ -172,32 +157,23 @@ trait FPBNodeTrait {
         self.members().uuid
     }
 
-    fn is_running(&self) -> bool {
-       self.members().state.load(Ordering::Relaxed) == true
+    fn is_running(&mut self) -> bool {
+        self.members().state.load(Ordering::Relaxed) == true
     }
 
-    // Accessor to get the sender channel
-    fn input_channel(&mut self) -> std::sync::mpsc::Sender<IIDMessage> {
-        self.members().sender
-    }
-
-    fn output_channel(&mut self) -> std::sync::mpsc::Receiver<IIDMessage> {
-        self.members().receiver
-    }
-
-    fn output_vetor(&mut self) -> std::vec::Vec<std::sync::mpsc::Sender<IIDMessage>> {
+    fn output_vector(&mut self) -> std::vec::Vec<std::sync::mpsc::Sender<Box<IIDMessage>>> {
         self.members().output_vec
     }
 
     // Post a message to the input queue
-    fn post_message(&mut self, msg: IIDMessage) {
-        let _ = self.members().sender.send(msg);
+    fn post_message(&mut self, msg: Box<IIDMessage>) {
+        let _ = self.members().tx.send(msg);
         return
     }
 
-    // Add an output node that will receive the output of 
-    // this node.  
-    fn add_receiver(&mut self, sender: std::sync::mpsc::Sender<IIDMessage>) {
+    // Add an output node that will receive the output of
+    // this node.
+    fn add_receiver(&mut self, sender: std::sync::mpsc::Sender<Box<IIDMessage>>) {
         self.members().output_vec.push(sender)
     }
 
@@ -205,37 +181,35 @@ trait FPBNodeTrait {
     // for each specific Node. The default processing is to simply write out
     // the input to the vector of output receivers.
     fn process_data(&mut self) {
-        // Get the next message to process.  NOTE: This will block if there
-        // are no items to process until a message is added to the input channel
-        let msg = self.output_channel().recv().unwrap();
-        for sender in self.output_vetor() {
-            sender.send(msg);
+        let msg_result = self.members().rx.recv();
+        if msg_result.is_err() {
+            return
+        }
+
+        let msg = msg_result.unwrap();
+
+        for sender in self.members().output_vec {
+            let msg_to_process = msg.clone();
+            sender.send(msg_to_process);
         }
     }
 
-    // Start the node to beginning processing.  If the node has already
-    // been started then this method will do nothing.
-    fn start(&self) {
-        if !self.is_running() {
-            *self.members().state.get_mut() = true;
-            let loop_condition = self.members().state.load(Ordering::Relaxed);
-            let fpb_trait = FPBNodeTrait;
-            let process_data_callback = || fpb_trait.process_data();
-            let handler = thread::spawn( move || {
-                while loop_condition {
-                    process_data_callback();
-                    loop_condition = self.members().state.load(Ordering::Relaxed);
-                } 
-            });
-
-            self.members().node_thread = Some(handler);
-
-
-          
+    fn start(mut self) where Self: 'static + std::marker::Sized + std::marker::Send {
+        if self.is_running() {
+            return
         }
+        let handler = thread::spawn(move || {
+            let mut loop_condition = true;
+            while loop_condition {
+                self.process_data();
+                loop_condition = self.is_running();
+            }
+        });
+
+        self.members().node_thread = Some(handler);
     }
 
-    // Stop all processing for this node.  It remains to be seen 
+    // Stop all processing for this node.  It remains to be seen
     // if this function will block until all currently enqueued message
     // will be processed, or all unprocessed message will be ignored.
     fn stop(&mut self) {
@@ -244,8 +218,8 @@ trait FPBNodeTrait {
             self.members().node_thread.unwrap().join();
             self.members().node_thread = None;
         }
-    }  
-   
+    }
+
 }
 
 #[cfg(test)]
